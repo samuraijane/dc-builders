65caedd0babcf572a528e1031b08c328405ae94b
Caitlin Crawford <caitlin.crawford16@gmail.com>
2020-08-12 15:44:02 -0400

    Add class exercises 1-3

--- week6/1-tuesday/class/exercise2/script.js
+++ week6/1-tuesday/class/exercise2/script.js
@@ -1,1 +1,15 @@
 
+// to get in ABC order: pull the input entries into an array and then use the .sort method
+// not working due to the fact that not pushing the array into the DOM?!
+
+let movieGenres = [];
+
+function genresArray() {
+    genreVal = document.getElementByName("movie-question").value;
+    movieGenres.push(genreVal);
+}
+
+genresArray();
+
+movieGenres.sort();
+
