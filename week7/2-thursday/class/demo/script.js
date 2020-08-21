4f9b504cf7fb72750f619cd85119ba096902fadb
Robert Powell <r.powelliv@gmail.com>
2020-08-20 22:06:33 -0400

    HWweek7

--- week7/2-thursday/class/demo/script.js
+++ week7/2-thursday/class/demo/script.js
@@ -26,3 +26,54 @@ button.addEventListener('click', function(e){
 
 
   //set a promise to prevent button enabling until done
+  let form = document.getElementById("formcoookie");
+
+
+// form.addEventListener("submit", function(e){
+//     e.preventDefault();
+//     let subbut = document.getElementById("submit-button");
+//     subbut.disabled=true;
+//     let newDiv = document.createElement('div');
+//     newDiv.innerHTML = "Please wait while we review your order..."
+//     form.appendChild(newDiv);
+
+//     console.log(e);
+//     const data = new FormData(form);
+//     console.log(stringifyFormData(data));
+//     const stringified = stringifyFormData(data);
+
+//     let dataInfo = data.forEach(listIt);
+
+//     function listIt(item, index) {
+//         newDiv.innerHTML += index + ": " + item + "<br />";
+//     } 
+
+//     postData(stringified).then(data => {
+//         subbut.disabled=false;
+//         console.log(`Your form has been submitted with the following data.\n\n${data}`)
+//         let dataInfo = data.forEach(listIt);
+//         function listIt(item, index) {
+//             newDiv.innerHTML += index + ":" + item + "<br>"; 
+//         }
+//         newDiv.innerHTML = `Here is your order: ${dataInfo}`;
+//     }
+// );
+// });
+
+// function stringifyFormData(fd) {
+//     const data = {};
+//     for (let key of fd.keys()){
+//         data[key] = fd.get(key);
+//     }
+//     return JSON.stringify(data,null,4);
+// };
+
+
+// const postData = (data) => {
+//     const promise = new Promise((res, rej) => {
+//         setTimeout(() => {
+//             res(data);
+//         }, 3000);
+//     });
+//     return promise;
+}
