0695a3c2e418bfb54184179e53dfdd69e1f6f57e
Alexia Allaway <alexiaallaway@gmail.com>
2020-08-22 10:04:33 -0400

    update1

--- week7/2-thursday/class/exercise1/script.js
+++ week7/2-thursday/class/exercise1/script.js
@@ -1,15 +1,26 @@
 const button = document.getElementById('button')
 
+document.addEventListener("DOMContentLoaded", function(data){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=ffe2cd367264d64431346733cc7afb14');
+    console.log(data);
+})
+
 button.addEventListener('click', function(){
     //console.log("Hello World");
     button.value="Generating Doggo...";
-    axios.get('https://dog.ceo/api/breeds/image/random')
-        .then(function(data){
-            const images = document.getElementById("images");
-            const img = document.createElement("IMG");
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(data => {
+            //const breeds = [];
+            //breeds.getElementById("breed");
+            const images = document.getElementById("dog_images");
+            const img = document.createElement("img");
+            img.setAttribute('src',data.message);
             images.appendChild(img);
-            images.innerHTML = data;
+            button.value = "Generate Doggo";
+
             
+            console.log(data);
         })
 
     
