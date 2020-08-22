1a645be56e1862d8abe73665c794777e7ef3f140
Stacey Graham <stacey.graham1106@gmail.com>
2020-08-21 23:43:45 -0400

    adding .then function and call back function to js file

--- week7/2-thursday/class/exercise1/script.js
+++ week7/2-thursday/class/exercise1/script.js
@@ -1,21 +1,30 @@
-// document.getElementById("button").addEventListener("click", function() {
-//     document.getElementById("dog-button").innerHTML = "Generate Doggo";
-//     console.log("dog-button")
-// }
-
-document.addEventListener("DOMContentLoaded", function(){
-    // Handler when the DOM is fully loaded
+document.getElementById("dog-button").addEventListener("click", function() {
+    document.getElementById("dog-button").innerHTML = "Generate Doggo...";
+    // console.log("dog-button")
     fetch('https://dog.ceo/api/breeds/image/random')
-        .then(response => response.json())
+        
+        .then(function(data) {
+
+        })
         .then(data => {
-            var dataCont = document.getElementById('dog-button');
-            var item = document.createElement("image");
-            // var text = data.weather[0].description;
-            item.innerHTML = JSON.stringify(data);
-            // item.innerHTML = text;
-            dataCont.appendChild(item);
-            console.log(data);
-        } );
-  });
+            
+
+        });
+});
+
+// document.addEventListener("DOMContentLoaded", function(){
+//     // Handler when the DOM is fully loaded
+//     fetch('https://dog.ceo/api/breeds/image/random')
+//         .then(response => response.json())
+//         .then(data => {
+//             var dataCont = document.getElementById('dog-button');
+//             var item = document.createElement("image");
+//             // var text = data.weather[0].description;
+//             dataCont.innerHTML = JSON.stringify(data);
+//             // item.innerHTML = text;
+//             // dataCont.appendChild(item);
+//             // console.log(data);
+//         } );
+//   });
 
 
