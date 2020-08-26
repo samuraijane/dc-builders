c73cefb082fef1443c6ec1855cfdbbab36e7c537
Alexandra Kasper <alexandrakasper@Alis-MacBook-Pro.local>
2020-08-25 21:09:44 -0400

    update scene it

--- week7/3-saturday/class/exercise1/scene-it-starter/index.js
+++ week7/3-saturday/class/exercise1/scene-it-starter/index.js
@@ -1,13 +1,31 @@
-$().ready(() => {
+$(function(){
 
-    function renderMovie(movieArray) {
-        movieHTML = movieArray.map(movie => {
-            return `<p>${movie.Title}</p>`
-            console.log(movie.Title);
-        });
+	function renderMovies(movieArray) {
 
-        $(".results").html(movieHTML.join(" "));
-    }
+		var finalHTML = "";
 
-    renderMovie(movieData);
-});
+		movieArray.forEach(function(currentMovie){
+
+			finalHTML += '<div class="card" style="width: 20rem;">';
+			finalHTML += '<img class="card-img-top" src="'+ currentMovie.Poster +'"/>';
+			finalHTML += '<div class="card-body">';
+			finalHTML += '<h4 class="card-title">'+ currentMovie.Title;
+			finalHTML += '<span class="badge badge-secondary float-right">'+ currentMovie.Year +'</span>';
+			finalHTML += '</h4>';
+			finalHTML += '<a href="#" class="btn btn-primary addButton">Add!</a>';
+			finalHTML += '</div>';
+			finalHTML += '</div>';
+		});
+
+		return finalHTML;
+	}
+
+	$('form').submit(function(e){
+		e.preventDefault();
+		var movieHTML = renderMovies(movieData);
+		$('.movie-container').html(movieHTML);
+	});
+
+
+
+})
