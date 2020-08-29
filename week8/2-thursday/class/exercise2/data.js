84edbd238d36b78a5b5e00b49aac7738407628d4
Robert <r.powelliv@gmail.com>
2020-08-29 07:54:37 -0700

    RobP

--- week8/2-thursday/class/exercise2/data.js
+++
@@ -1,7 +0,0 @@
-var urls = [
-    'https://dog.ceo/api/breed/beagle/images/random',
-    'https://dog.ceo/api/breed/chow/images/random',
-    'https://dog.ceo/api/breed/akita/images/random',
-    'https://dog.ceo/api/breed/dingo/images/random',
-    'https://dog.ceo/api/breed/eskimo/images/random'
-  ];
--- week8/2-thursday/class/exercise2/index.js
+++ week8/2-thursday/class/exercise2/index.js
@@ -1,9 +1,64 @@
-async function get () {
-   let r1 = await fetch('https://dog.ceo/api/breed/beagle/images/random')
-   let r2 = await fetch('https://dog.ceo/api/breed/chow/images/random')
-   let r3 = await fetch('https://dog.ceo/api/breed/akita/images/random')
-   let r4 = await fetch('https://dog.ceo/api/breed/dingo/images/random')
-   let r5 = await fetch('https://dog.ceo/api/breed/eskimo/images/random')
-   let data = 'data was fetched!'
-   console.log(data);
-};
+var urls = [
+    'https://dog.ceo/api/breed/beagle/images/random',
+    'https://dog.ceo/api/breed/chow/images/random',
+    'https://dog.ceo/api/breed/akita/images/random',
+    'https://dog.ceo/api/breed/dingo/images/random',
+    'https://dog.ceo/api/breed/eskimo/images/random'
+  ];
+
+//one after another
+
+// async function get () {
+//    let r1 = await fetch(urls[0])
+//    let r2 = await fetch('https://dog.ceo/api/breed/chow/images/random')
+//    let r3 = await fetch('https://dog.ceo/api/breed/akita/images/random')
+//    let r4 = await fetch('https://dog.ceo/api/breed/dingo/images/random')
+//    let r5 = await fetch('https://dog.ceo/api/breed/eskimo/images/random')
+//    console.log('data 5 was fetched!')
+   
+// };
+
+// fetch(urls[0])
+// .then(response => {
+//     console.log('data 1 was fetched...')
+//     fetch(urls[1]).then( response => {
+//         console.log('data 1 was fetched...')    
+//     });
+// })
+
+//.Promise.all()
+//waits to all are done
+
+let promiseArray = urls.map((url, indx) => {
+    return fetch(url).then( () => console.log(`Data #${indx}`))
+});
+
+Promise.all(promiseArray).then(() => console.log ('All done'));
+
+
+//Add numbers
+
+   let addNumbers = (x , y) => {
+        return new Promise((resolve, reject) => {
+            if(typeof x !== 'number' || typeof y !== 'number') {
+                reject('X or Y is not a number')
+            };
+            resolve(x+y);
+       });
+   }
+
+   addNumbers(1, 2)
+   .then(function (answer) {
+      console.log(answer);
+   })
+   .catch(function (error) {
+      console.log(error);
+   });
+
+   addNumbers(1, 'dog')
+   .then(function (answer) {
+      console.log(answer);
+   })
+   .catch(function (error) {
+      console.log(error);
+   });
