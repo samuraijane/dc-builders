6299599ea81e1072a609fa01cc29ede952cc4bad
ejw773 <ejw773@gmail.com>
2020-08-31 23:02:56 -0400

    adds Hero vs. Villain functionality

--- week8/3-saturday/class/exercise2/elijah-hero-vs-villain/scripts/Hero.js
+++ week8/3-saturday/class/exercise2/elijah-hero-vs-villain/scripts/Hero.js
@@ -6,3 +6,5 @@ class Hero extends Person{
 
     }
 }
+
+// https://images-na.ssl-images-amazon.com/images/G/01/digital/video/hero/Movies/2002/B00742BVW8_Hero._V142683513_SX1080_.jpg
--- week8/3-saturday/class/exercise2/elijah-hero-vs-villain/scripts/Villain.js
+++ week8/3-saturday/class/exercise2/elijah-hero-vs-villain/scripts/Villain.js
@@ -6,3 +6,5 @@ class Villain extends Person{
 
     }
 }
+
+// https://images.squarespace-cdn.com/content/v1/54faf78ce4b04da0abdfbde8/1527192850299-O1TSEYT141PKYNL24N6S/ke17ZwdGBToddI8pDm48kBpiV9Y7ITPTlwOYCI_cgaIUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYy7Mythp_T-mtop-vrsUOmeInPi9iDjx9w8K4ZfjXt2dshq0IZLe6nQkHkUnHBavr9sqwwuMPavG2yLPftpXG72ZDqXZYzu2fuaodM4POSZ4w/hero-2002-389.jpg
