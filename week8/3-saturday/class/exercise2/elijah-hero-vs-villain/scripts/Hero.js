d727d5c890b58bcfb97f49011faf8742aa3cdd1b
ejw773 <ejw773@gmail.com>
2020-09-01 07:12:11 -0400

    makes weapon functional

--- week8/3-saturday/class/exercise2/elijah-hero-vs-villain/scripts/Hero.js
+++ week8/3-saturday/class/exercise2/elijah-hero-vs-villain/scripts/Hero.js
@@ -3,7 +3,8 @@ class Hero extends Person{
         super(inputName);
     }
     render(heroName) {
-        let heroHTML = `<h2 class="player-name" id="hero-name">The Hero: ${heroName}</h2><img src="https://fromthebalcony.com/wp-content/uploads/2014/01/Hero_anexdu.jpg"><button class="attack" id="attack-the-hero">Attack the Hero!</button><h3>Life Remaining:</h3><p class="life" id="hero-life">100</p>`
+        let heroHTML = `<h2 class="player-name" id="hero-name">The Hero: ${heroName}</h2><img src="https://fromthebalcony.com/wp-content/uploads/2014/01/Hero_anexdu.jpg"><button class="attack" id="attack-the-villain" onclick="attackVillain()">Attack the Villain! --></button><h3>Life Remaining:</h3><p class="life" id="hero-life">100</p>`
         document.getElementById("hero-container").innerHTML = heroHTML;
     }
+
 }
--- week8/3-saturday/class/exercise2/elijah-hero-vs-villain/scripts/script.js
+++ week8/3-saturday/class/exercise2/elijah-hero-vs-villain/scripts/script.js
@@ -5,8 +5,28 @@ window.addEventListener("load", function() {
     newHero.render(heroName);
     newVillain = new Villain(villainName);
     newVillain.render(villainName);
-    attackVillain = document.getElementById("attack-the-villain");
-    attackHero = document.getElementById("attack-the-hero");
-    // attackVillain.addEventListener("click", );
-    // attackHero.addEventListener("click", );
 })
+
+function attackHero() {
+    document.getElementById("villain-container").style.backgroundColor = "white";
+    document.getElementById("hero-container").style.backgroundColor = "red";
+    let attackHeroWeapon = new Weapon;
+    let heroLife = document.getElementById("hero-life");
+    lowerLife = heroLife.innerHTML - attackHeroWeapon.damage;
+    heroLife.innerHTML = lowerLife;
+    if (heroLife.innerHTML < 1) {
+        alert("Villain wins!")
+    }
+}
+
+function attackVillain() {
+    document.getElementById("hero-container").style.backgroundColor = "white";
+    document.getElementById("villain-container").style.backgroundColor = "red";
+    let attackVillainWeapon = new Weapon;
+    let villainLife = document.getElementById("villain-life");
+    lowerLife = villainLife.innerHTML - attackVillainWeapon.damage;
+    villainLife.innerHTML = lowerLife;
+    if (villainLife.innerHTML < 1) {
+        alert("Hero wins!")
+    }
+}
