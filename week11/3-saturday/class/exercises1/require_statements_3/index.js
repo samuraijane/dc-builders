00749a19e96405d4415d3ecbacd7452d32e49f63
ejw773 <ejw773@gmail.com>
2020-09-19 11:36:49 -0400

    completes Saturday exercises

---
+++ week11/3-saturday/class/exercises1/require_statements_3/index.js
@@ -0,0 +1,5 @@
+const array = require('./array.js');
+
+for (var i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
---
+++ week11/3-saturday/class/exercises1/require_statements_4/index.js
@@ -0,0 +1,3 @@
+const addThis = require('./add');
+
+console.log(addThis(123, 321));
