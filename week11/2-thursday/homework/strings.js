4362e00033516f3e9cdf71f709bd2ecf8fc5f60b
JohnLoftin <jgloftin@gmail.com>
2020-09-19 11:12:16 -0400

    added thursday exercises

--- week11/2-thursday/homework/strings.js
+++ week11/2-thursday/homework/strings.js
@@ -1,10 +1,14 @@
 class Strings {
     // Reverses a string.
     reverse(toReverse) {
+        let split = toReverse.split("");
+        split = split.reverse();
+        return split.join(""); 
     }
 
     // Detectes if a string is a pallindrome
     detectPallindrome(toCheck) {
+        return toCheck === this.reverse(toCheck);
     }
 
     // Shifts every letter forward by one. Z goes
@@ -14,6 +18,7 @@ class Strings {
 
     // Checks to see if the target string is a number
     isNumber(toCheck) {
+        return typeof toCheck === "Number";
     }
 }
 
