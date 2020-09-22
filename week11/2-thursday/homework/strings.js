1ed7db3e6789d0f07ae0716dbadc83dda355f407
Caitlin Crawford <caitlin.crawford16@gmail.com>
2020-09-22 13:22:24 -0400

    Add more hw exercise files

--- week11/2-thursday/homework/strings.js
+++ week11/2-thursday/homework/strings.js
@@ -1,19 +1,27 @@
 class Strings {
     // Reverses a string.
     reverse(toReverse) {
+        let strSplit = toReverse.split("");
+        let reverseArr = strSplit.reverse();
+        let arrJoin = reverseArr.join("");
+        return arrJoin;
     }
 
     // Detectes if a string is a pallindrome
     detectPallindrome(toCheck) {
+        return toCheck === this.reverse(toCheck);  //reverse function that belongs to the class (need this keyword)
     }
 
     // Shifts every letter forward by one. Z goes
     // to A (wraps around)
     shift(toShift) {
+        let chars = toShift.split("")
+        // like Ceasar Cipher; use charcode stuff
     }
 
     // Checks to see if the target string is a number
     isNumber(toCheck) {
+        return typeof toCheck === "Number"
     }
 }
 
