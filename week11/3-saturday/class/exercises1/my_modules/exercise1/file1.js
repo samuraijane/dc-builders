ec37d504f1167748c6a2690fc775a9cc6f4e5077
Stacey Graham <stacey.graham1106@gmail.com>
2020-09-22 18:23:57 -0400

    adding solutions to exercises

---
+++ week11/3-saturday/class/exercises1/my_modules/exercise1/file1.js
@@ -0,0 +1,3 @@
+// 1) Write a file that exports the number three. Write a file that exports the number five. Write a program that requires those files and console logs the sum of three and five
+module.exports = 3;
+
---
+++ week11/3-saturday/class/exercises1/my_modules/exercise1/index.js
@@ -0,0 +1,4 @@
+const file1 = require("./file1");
+const file2 = require("./file2");
+var sum = file1 + file2;
+console.log(sum); 
