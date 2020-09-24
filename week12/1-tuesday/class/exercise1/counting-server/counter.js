be5883aa2e6b57cb0b656dc1edff99979cbdf370
Stacey Graham <stacey.graham1106@gmail.com>
2020-09-24 18:58:42 -0400

    adding my solution

--- week12/1-tuesday/class/exercise1/counting-server/counter.js
+++ week12/1-tuesday/class/exercise1/counting-server/counter.js
@@ -4,8 +4,20 @@ const express = require('express')
 
 const app = express()
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.post('/count', function (req, res) {
+  console.log("I'm a server, and I got a new request for a list.")
+  count += 1
+  res.send(`${count} requests received`)
+})
+
+app.post('/count', function (req, res) {
+  console.log("I'm a server, and I got a new request for a list.")
+  count -= 1
+  res.send(`${count} requests received`)
 })
  
+app.get('/count', function (req, res) {
+  res.send(`${count}`)
+})
+
 app.listen(3000)
