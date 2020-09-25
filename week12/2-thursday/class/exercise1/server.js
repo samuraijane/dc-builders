dfd117658912e479732b2176716714fed0795c6b
Daniel Gallagher <danielbgallagher@gmail.com>
2020-09-24 20:41:02 -0400

    requests

--- week12/2-thursday/class/exercise1/server.js
+++ week12/2-thursday/class/exercise1/server.js
@@ -13,14 +13,55 @@ var todoList = [
 ];
 
 // GET /api/todos
-
+app.get('/api/todos', (req, res) => {
+    res.send(todoList);
+})
 // GET /api/todos/:id
+app.get('/api/todos/:id', (req, res) => {
+    let result = todoList.filter( (todoItem) => {
+        return todoItem.id === parseInt(req.params.id)
+    })
 
+    if (result.length === 1){
+        res.send(result[0]);
+    } else if(result.length === 0){
+        res.status(404).send("Error: todo item not found")
+    } else if(result.length > 1){
+        res.status(500).send("Error: too many todos")
+    }
+
+})
 // POST /api/todos
 
-// PUT /api/todos/:id
+app.post('/api/todos', (req, res) => {
+    let newItem = req.body;
+    newItem["id"] = autoIncrement++;
 
+    todoList.push(req.body)
+})
+// PUT /api/todos/:id.
+app.put('api.todos/:id', (req,res) => {
+    
+
+    for (let i = 0; i < todoList.length; i++ ){
+        if(todoList[i].id === parseInt(req.params.id)){
+            todoList[i]["isCompleted"] = req.body.isCompleted
+            todoList[i]["todo"] = req.body.todo
+            res.send(todoList[i])
+        }
+    }
+    res.status(404).send("Item not found");
+})
 // DELETE /api/todos/:id
+app.delete('api/todos/:id', (req,res)=>{
+    let originalLength = todoList.length
+
+    todoList.filter((todoItem)=>{
+        return todoItem.id !== parseInt(req.params.id)
+
+    })
+    res.send();
+})
 
 app.listen(3000, function(){
     console.log('Todo List API is now listening on port 3000...');
