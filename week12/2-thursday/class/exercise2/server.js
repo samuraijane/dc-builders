8a1680a25d2e755eabc8121d5b2e7deb213a9038
JohnLoftin <jgloftin@gmail.com>
2020-09-26 10:04:05 -0400

    added week12 thursday exercises

--- week12/2-thursday/class/exercise2/server.js
+++ week12/2-thursday/class/exercise2/server.js
@@ -35,18 +35,71 @@ var owners = [
     }
 ];
 
+let autoIncrement = 3;
+
 
 // GET /api/owners
+app.get('/api/owners', (req, res) => {
+    res.send(owners);
+})
 
 // GET /api/owners/:id
+app.get('/api/owners/:id', (req, res) => {
+
+    let result = owners.filter((ownerItem) => {
+        return ownerItem.id === parseInt(req.params.id);
+    })
+    if(result.length === 1){
+        res.send(result[0]);
+    } else if(result.length === 0){
+        res.status(404).send("Todo list item does not exist");
+    } else {
+        res.status(500).send("The server had an error: to many todos");
+    }
+})
 
 // POST /api/owners
+app.post('/api/owners', (req, res) => {
+    let newItem = req.body;
+    newItem["id"] = autoIncrement++;
+    owners.push(newItem);
+    res.send(newItem);
+})
 
 // PUT /api/owners/:id
+app.put('/api/owners/:id', (req, res) => {
+    for(let idx = 0; idx < owners.length; idx++) {
+        console.log(owners[idx])
+        if(owners[idx].id === parseInt(req.params.id)){
+            itemFound = true;
+            owners[idx]["ownsPets"] = req.body.ownsPets
+            console.log("In the loop")
+            res.send(owners[idx]);
+        }
+    }
+
+    res.status(404).send("404 - Item does not exist");
+})
 
 // DELETE /api/owners/:id
+app.delete('/api/owners/:id', (req, res) => {
+    let originalLength = owners.length;
+
+    owners = owners.filter((ownerItem) => {
+        return ownerItem.id !== parseInt(req.params.id);
+    })
+
+    if(owners.length === originalLength){
+        res.send("Item already removed");
+    }
+
+    res.send("Deleted an item");
+})
 
 // GET /api/owners/:id/pets
+app.get('/api/owners/:id/pets', (req, res) => {
+    
+})
 
 // GET /api/owners/:id/pets/:petId
 
