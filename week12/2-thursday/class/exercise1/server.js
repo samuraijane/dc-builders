cafd6d30a76cf5a0952a46f8479ee46e115d5b2b
Alexandra Kasper <alexandrakasper@Alis-MacBook-Pro.local>
2020-09-29 06:40:46 -0400

    thursday homework

--- week12/2-thursday/class/exercise1/server.js
+++ week12/2-thursday/class/exercise1/server.js
@@ -12,15 +12,84 @@ var todoList = [
     }
 ];
 
+let autoIncrement = 2;
+
 // GET /api/todos
+app.get('/api/todos', (req, res) => {
+    res.send(todoList)
+})
 
 // GET /api/todos/:id
+app.get('/api/todos/:id', (req, res) => {
+    console.log(req.params.id)
+
+    let result = todoList.filter( (todoItem) => {
+        return todoItem.id === parseInt(req.params.id)
+    })
+
+    if (result.length === 1) {
+        res.send(result[0])
+    } 
+    else if (result.length === 0) {
+        res.status(404).send("todo list item doesn't exist")
+    }
+    else {
+        res.status(500).send("The server had an error: to many todos.")
+    }
+})
 
 // POST /api/todos
+app.post('/api/todos', (req, res) => {
+    console.log(req.body);
+    console.log(typeof req.body);
+
+    let newItem = req.body;
+    newItem["id"] = autoIncrement++;
+    
+    todoList.push(newItem);
+
+    res.send(newItem);
+})
 
 // PUT /api/todos/:id
+app.put('/api/todos/:id', (req, res) => {
+    console.log(req.body);
+    console.log(req.params.id);
+
+    let mutateItem;
+    for (let i = 0; i < todoList.length; i++) {
+        console.log(todoList[i]);
+
+        if (todoList[i].id === parseInt(req.params.id)) {
+            console.log("In the loop");
+            todoList[i]["isCompleted"] = req.body.isCompleted;
+            todoList[i]["todo"] = req.body.todo;
+            res.send(todoList[i]);
+        }
+    }
+
+    res.status(404).send("Item does not exist.");
+})
 
 // DELETE /api/todos/:id
+app.delete('/api/todos/:id', (req, res) => {
+
+    let originalLength = todoList.length;
+    
+    todoList = todoList.filter( (todoItem) => {
+        return todoItem.id !== parseInt(req.params.id);
+    })
+
+    console.log(todoList);
+
+    if (todoList.length === originalLength) {
+        res.send("Item already removed");
+    }
+    else {
+        res.send("Deleted this item");
+    }
+
+})
 
 app.listen(3000, function(){
     console.log('Todo List API is now listening on port 3000...');
--- week12/2-thursday/class/exercise2/server.js
+++ week12/2-thursday/class/exercise2/server.js
@@ -35,18 +35,85 @@ var owners = [
     }
 ];
 
+let autoIncrement = 3;
+
 
 // GET /api/owners
+app.get('/api/owners', (req, res) => {
+    res.send(owners);
+})
 
 // GET /api/owners/:id
+app.get('/api/owners/:id', (req, res) => {
+    console.log(req.params.id);
+
+    let result = owners.filter((owner) => {
+        return owner.id === parseInt(req.params.id);
+    })
+
+    res.send(result);
+})
 
 // POST /api/owners
+app.post('/api/owners', (req, res) => {
+    console.log(req.body);
+    console.log(typeof req.body);
+
+    let newOwner = req.body;
+    newOwner["id"] = autoIncrement++;
+
+    owners.push(newOwner);
+
+    res.send(newOwner);
+})
 
 // PUT /api/owners/:id
+app.put('/api/todos/:id', (req, res) => {
+    console.log(req.body);
+    console.log(req.params.id);
+
+    // for (let i = 0; i < owners.length; i++) {
+    //     console.log(owners[i]);
+
+    //     if (owners[i].id === parseInt(req.params.id)) {
+    //         console.log("In the loop");
+    //         todoList[i]["name"] = req.body.name;
+    //         todoList[i]["pets"] = req.body.pets;
+    //         res.send(todoList[i]);
+    //     }
+    // }
+
+    // res.status(404).send("Owner is not in database.");
+})
 
 // DELETE /api/owners/:id
+app.delete('/api/owners/:id', (req, res) => {
+
+    let originalLength = owners.length;
+
+    owners = owners.filter( (owner) => {
+        return owner.id !== parseInt(req.params.id);
+    })
+
+    console.log(owners);
+
+    if (owners.length === originalLength) {
+        res.send("Owner has already been removed.");
+    }
+    else {
+        res.send("Owner has been deleted.");
+    }
+})
 
 // GET /api/owners/:id/pets
+app.get('/api/owners/:id/pets', (req, res) => {
+
+    let result = owners.filter((owner) => {
+        return owner.id === req.params.id;
+    })
+
+    res.send(result);
+})
 
 // GET /api/owners/:id/pets/:petId
 
