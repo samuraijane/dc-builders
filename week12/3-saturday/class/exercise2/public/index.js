a80bd9f9195db0ba63e081354ce5551fca89bdf0
ejw773 <ejw773@gmail.com>
2020-09-29 18:30:02 -0400

    creates files for to do list

---
+++ week12/3-saturday/class/exercise2/public/index.js
@@ -0,0 +1,12 @@
+console.log("Hello world");
+const express = require('express')
+const app = express()
+
+
+$().ready(()=> {
+    // fetch('./api/to-do-list')
+    // .then(res => res.json())
+    // .then(data => {
+    //     $('#counter-value').html(data)
+    // })
+})
