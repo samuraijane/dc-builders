75993f5e60a389864f7a0cacc9d19cb550a3b5ad
Heather Luttrell <possumdiva@heatherluttrell.com>
2020-09-29 16:32:18 -0400

    saturday homework

---
+++ week12/3-saturday/class/exercise2/public/index.js
@@ -0,0 +1,22 @@
+let render = (dataArray) => {
+  let todoArray = dataArray.map((todoItem) => {
+    return `<li>${todoItem["todo"]} <a href="#" onclick="deleteItem(${todoItem["id"]})" id="trash-delete"><i class="fas fa-trash"></i></a></li>`;
+  });
+  return todoArray.join("\n");
+};
+let deleteItem = (itemID) => {
+  console.log(itemID);
+  fetch("/api/todos/:id", { method: "DELETE" })
+    .then((res) => res.json())
+    .then((data) => {
+      $("#todo-list").html(render(data));
+    });
+};
+$().ready(() => {
+  fetch("/api/todos")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      $("#todo-list").html(render(data));
+    });
+});
