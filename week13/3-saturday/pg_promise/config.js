dd4da46c2720f506bcde71521b906e290cded39d
ejw773 <ejw773@gmail.com>
2020-10-03 12:37:03 -0400

    class exercises

---
+++ week13/3-saturday/pg_promise/config.js
@@ -0,0 +1,5 @@
+exports.DB_NAME = process.env.DB_NAME;
+exports.DB_LOCAL = process.env.DB_LOCAL;
+exports.DB_PASSWORD = process.env.DB_PASSWORD;
+exports.PORT = 3001;
+exports.DB_USER = process.env.DB_USER;
