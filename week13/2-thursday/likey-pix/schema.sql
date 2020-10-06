4eea3df7ae1db7cf57b59d36597a685b7d0f7256
Stacey Graham <stacey.graham1106@gmail.com>
2020-10-06 06:40:06 -0400

    updating schema.sql with on delete cascade statements

--- week13/2-thursday/likey-pix/schema.sql
+++ week13/2-thursday/likey-pix/schema.sql
@@ -7,7 +7,7 @@ create table users (
 create table posts (
     id serial primary key,
     url text not null,
-    user_id integer references users (id)
+    user_id integer references users (id) on delete cascade
 );
 
 create table tags (
@@ -16,24 +16,19 @@ create table tags (
 );
 
 create table tags_posts (
-    tag_id integer references tags (id),
-    post_id integer references posts (id)
+    tag_id integer references tags (id) on delete cascade,
+    post_id integer references posts (id) on delete cascade
 );
 
 create table likes (
-    user_id integer references users (id),
-    post_id integer references posts (id)
-);
-
-create table comments (
-    post_id integer references posts (id),
-    user_id integer references users (id)
+    user_id integer references users (id) on delete cascade,
+    post_id integer references posts (id) on delete cascade
 );
 
 create table comments (
     id serial primary key,
     comment text not null,
     created_at timestamp default now(),
-    post_id integer references posts (id),
-    user_id integer references users (id)
+    post_id integer references posts (id) on delete cascade,
+    user_id integer references users (id) on delete cascade
 );
