28b968e9ea89f45bc740605f78d9b0505eb0aa1a
Matthew Day <matt@matthewday.net>
2020-10-06 14:15:03 -0400

    Add Week 13 Thursday

---
+++ week13/2-thursday/demo/seed.sql
@@ -0,0 +1,48 @@
+insert into users
+  (name, email)
+values
+  ('Alice', 'alice@email.com'),
+  ('Bob', 'bob@email.com'),
+  ('Cho', 'cho@email.com'),
+  ('Daryl', 'daryl@email.com'),
+  ('Emmy', 'emmy@email.com')
+;
+
+insert into posts
+  (url, user_id)
+values
+  ('walking-the-cat.jpg', 2),
+  ('day-at-the-beach.jpg', 3),
+  ('new-puppy.jpg', 1),
+  ('cat-in-a-box.jpg', 5),
+  ('doggos.jpg', 5)
+;
+
+insert into tags
+  (tag)
+values
+  ('cute'),
+  ('fluffy'),
+  ('whorescuedwho')
+;
+
+insert into tags_posts
+  (tag_id, post_id)
+values
+  (1, 2),
+  (2, 2),
+  (1, 4),
+  (3, 3),
+  (3, 5)
+;
+
+insert into likes
+  (user_id, post_id)
+values
+  (1, 1),
+  (1, 2),
+  (2, 3),
+  (4, 5),
+  (2, 2),
+  (3, 1)
+;
