0ef6434163944d9825ca30a916a10429647144c4
Stacey Graham <stacey.graham1106@gmail.com>
2020-10-06 18:38:47 -0400

    adding changes to todo-crud-app

--- week13/3-saturday/todo-crud-api/server.js
+++ week13/3-saturday/todo-crud-api/server.js
@@ -3,5 +3,35 @@ const express = require("express");
 const app = express();
 const pgp = require("pg-promise")();
 
-const db = pgp("postgres://username:password@host:port/database");
+const db = pgp("postgres://shayla:H3!!0p33ps@host:5432/database");
+
+//POST CREATE A TASK
+app.POST('/tasks', (req, res) => {
+    res.send()
+})
+
+
+//GET TASKS
+app.get('/tasks', (req, res) => {
+
+})
+
+//PATCH /tasks/:id/is_completed
+app.patch('/tasks/:id/is_completed', (req, res) => {
+
+})
+
+//PATCH /tasks/:id/title
+app.patch('tasks/:id/title', (req, res) => {
+
+})
+
+// DELETE /tasks/:id
+app.delete('/tasks/:id', (req, res) => {
+
+})
+
+
+
+
 
