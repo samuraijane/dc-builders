f25a248537de26c4e23e9b6e6a8835593019d571
Stacey Graham <stacey.graham1106@gmail.com>
2020-10-06 19:41:45 -0400

    updating solution from class

--- week13/3-saturday/todo-crud-api/server.js
+++ week13/3-saturday/todo-crud-api/server.js
@@ -2,22 +2,27 @@
 const express = require("express");
 const app = express();
 const pgp = require("pg-promise")();
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
 
 const db = pgp("postgres://shayla:H3!!0p33ps@host:5432/database");
 
 //POST CREATE A TASK
-app.POST('/tasks', (req, res) => {
-    res.send()
-})
+app.post('/tasks', function (req, res) {
+    const title = req.body.title; //Insert into tasks into the title column 
+    db.none("INSERT INTO tasks (title) VALUES ($1)", [newTaskTitle]).then(() => {
+        res.send(`Tasks "${newTaskTitle}" was created`);
+    });
+});
 
 
 //GET TASKS
-app.get('/tasks', (req, res) => {
+app.get('/tasks', function (req, res) {
 
 })
 
 //PATCH /tasks/:id/is_completed
-app.patch('/tasks/:id/is_completed', (req, res) => {
+app.patch('/tasks/:id/is_completed', function (req, res) {
 
 })
 
@@ -30,6 +35,10 @@ app.patch('tasks/:id/title', (req, res) => {
 app.delete('/tasks/:id', (req, res) => {
 
 })
+
+app.listen(PORT, () => {
+    console.log(`Express application is running on port ${PORT}`);
+});
 
 
 
