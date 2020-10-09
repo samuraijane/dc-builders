24cd4f2ebd1df595019304f4abfdda5533f12887
Stacey Graham <stacey.graham1106@gmail.com>
2020-10-09 13:53:57 -0400

    updated code to console.log the input values

--- week14/2-thursday/exercises/ex1/index.js
+++ week14/2-thursday/exercises/ex1/index.js
@@ -1,3 +1,17 @@
-alert('hey');
-const form = document.getElementById('form');
-form.addEventListener('submit', handleSubmit);
+
+    const title = document.getElementById('title');
+    const author = document.getElementById('author');
+    const publicationDate = document.getElementById('publicationDate');
+    
+    form.addEventListener('submit', function (e){
+        e.preventDefault();
+
+        console.log(title.value);
+        console.log(author.value);
+        console.log(publicationDate.value);
+
+
+    }); 
+
+
+
