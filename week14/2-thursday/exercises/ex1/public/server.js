e02ea57f4ef0b0f2996d1bd1db22e640ff798080
Stacey Graham <stacey.graham1106@gmail.com>
2020-10-09 17:09:13 -0400

    moving files and updating html

---
+++ week14/2-thursday/exercises/ex1/public/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public')); 
+
+app.post("/books", function(req, res) {
+	res.json({
+        api: "hello world"
+    });
+});
+
+app.listen(3001, function() {
+    console.log("The server at port 3001 is listening.");
+});
