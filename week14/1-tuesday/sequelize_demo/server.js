f4868ff2e81fd72c8be506abe92899422049dd2e
Caitlin Crawford <caitlin.crawford16@gmail.com>
2020-10-10 09:06:24 -0400

    Edit server.js for sequelize

--- week14/1-tuesday/sequelize_demo/server.js
+++ week14/1-tuesday/sequelize_demo/server.js
@@ -12,6 +12,7 @@ const { User } = require('./models');
 const apiUser = require("./api/apiUser");
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/heartbeat", function(req, res) {
   res.json({
