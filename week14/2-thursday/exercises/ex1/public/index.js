b0bc329a8edfc6cbb5072f798cf9be24351eebed
Stacey Graham <stacey.graham1106@gmail.com>
2020-10-10 11:20:42 -0400

    updating html

--- week14/2-thursday/exercises/ex1/public/index.js
+++ week14/2-thursday/exercises/ex1/public/index.js
@@ -1,17 +1,32 @@
 
-    const title = document.getElementById('title');
-    const author = document.getElementById('author');
-    const publicationDate = document.getElementById('publicationDate');
-    
-    form.addEventListener('submit', function (e){
+$().ready(()=>{
+    function stringifyFormData(fd){
+        const data={};
+        for(let field of fd.keys()){
+            data[field]=fd.get(field)
+        }
+        return JSON.stringify(data,null,2);
+    }
+    async function postData(url,body){
+        const response=await fetch(url,{
+            method:'POST',
+            headers:{
+                'Content-Type':'application.json'
+            },
+            body
+        })
+        return response.json();
+    }
+    $('#form').submit((e)=>{
         e.preventDefault();
-
-        console.log(title.value);
-        console.log(author.value);
-        console.log(publicationDate.value);
-
-
-    }); 
+        const data=new FormData(e.target);
+        const stringified=stringifyFormData(data);
+        postData('https://localhost:3001/books',stringified);
+        // console.log(`title is ${title} and author is ${author} and it was published on ${pubDate}!`)
+        // postData('localhost:3001/books',stringified)
+        // .then(data =>console.log('data',data))
+    })
+});
 
 
 
