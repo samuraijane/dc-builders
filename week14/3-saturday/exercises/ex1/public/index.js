7d79a8a637ec7f35adcbd927e4895ab0761da76e
Matthew Day <matt@matthewday.net>
2020-10-11 20:56:49 -0400

    Add an event listener that logs to the console captured input

--- week14/3-saturday/exercises/ex1/public/index.js
+++ week14/3-saturday/exercises/ex1/public/index.js
@@ -1,1 +1,18 @@
-alert('hello world');
+function stringifyFormData(fd) {
+    const data = {};
+    for (let field of fd.keys()) {
+      data[field] = fd.get(field);
+    }
+    return JSON.stringify(data, null, 2);
+  }
+  
+  const handleSubmit = e => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const stringified = stringifyFormData(data);
+    console.log(stringified);
+  }
+  
+  const form = document.getElementById('form');
+  form.addEventListener('submit', handleSubmit);
+  
