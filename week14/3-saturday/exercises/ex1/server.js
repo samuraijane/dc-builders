9f428ca09799bf3f89071994a92bcf2b482d4c44
Matthew Day <matt@matthewday.net>
2020-10-11 20:43:31 -0400

    Add heartbeat endpoint to make sure things are working as expected

--- week14/3-saturday/exercises/ex1/server.js
+++ week14/3-saturday/exercises/ex1/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.get("/heartbeat", function(req, res) {
+    res.json({
+        is: "working"
+    });
+});
+
 
 app.listen(3001, function() {
   console.log('The server at port 3001 is listening.');
