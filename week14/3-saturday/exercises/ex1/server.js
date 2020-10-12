6a08c59ae7878eaba9ff89ffcab190156089bc15
Matthew Day <matt@matthewday.net>
2020-10-11 20:49:57 -0400

    Add index.html with simple form

--- week14/3-saturday/exercises/ex1/server.js
+++ week14/3-saturday/exercises/ex1/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get("/heartbeat", function(req, res) {
     res.json({
         is: "working"
