063cd4831c415c7a00d0e2fa9df579ff7a427ee7
Stacey Graham <stacey.graham1106@gmail.com>
2020-10-13 18:12:33 -0400

    adding javascript code to index.js

--- week12/3-saturday/class/exercise2/public/index.js
+++ week12/3-saturday/class/exercise2/public/index.js
@@ -1,4 +1,35 @@
 console.log("Hello World")
 $().ready(()=> {
-    fetch('localhost:3000/api/')
+    // fetch('localhost:3000/api/')
+//Selectors
+document.querySelector('form').addEventListener('submit', handleSubmitForm);
+
+//Event Handlers
+function handleSubmitForm(e) {
+    e.preventDefault();
+    let input = document.querySelector('input');
+    if (input.value != '')
+        addTodo(input.value);
+    input.value = '';
+}
+
+//Helpers
+function addTodo(todo) {
+    let ul = document.querySelector('ul');
+    let li = document.createElement('li');
+    li.innerHTML = `
+        <span class="todo-item">${todo}</span>
+        <button name="checkButton"><i class="fas fa-check-square"></i></button>
+        <button name="deleteButton" ><i class="fas fa-trash"></i></button>
+    `;
+    li.classList.add('todo-list-item');
+    ul.appendChild(li);
+}
+
+
+
+
+
+
+
 })
