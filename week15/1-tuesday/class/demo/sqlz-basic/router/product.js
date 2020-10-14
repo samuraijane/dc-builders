b7e64e07ae51052df2be0e20a43d91a5962fe179
DanStough <dan.stough@accesscfa.com>
2020-10-13 22:16:12 -0400

    Tuesday Class Examples

---
+++ week15/1-tuesday/class/demo/sqlz-basic/router/product.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const router = express.Router()
+
+const db = require('../models')
+
+router.get('/products', async (req, res)=>{
+    const products = await db.product.findAll()
+    res.send(products)
+})
+
+router.get('/products/:id', async (req, res)=>{
+    const productId = parseInt(req.params.id)
+    const product = await db.product.findByPk(productId)
+    console.log(product)
+
+    if(product) {
+        res.send(product)
+    } else {
+        res.status(404).send("Product does not exist")
+    }
+})
+
+router.put('/products/:id', async (req, res)=>{
+    const productId = parseInt(req.params.id)
+    const product = await db.product.findByPk(productId)
+
+    if(product) {
+        product.cost = req.body.cost
+        product.sale = req.body.sale
+        await product.save()
+        res.send(product)
+    } else {
+        res.status(404).send("Product does not exist")
+    }
+})
+
+router.post('/products', async (req, res)=>{
+    const {name, cost, stock, description, sale} = req.body
+
+    const newProduct = await db.product.create({
+        name,
+        cost,
+        stock,
+        description,
+        sale
+    })
+
+    res.send(newProduct)
+})
+
+module.exports = router
