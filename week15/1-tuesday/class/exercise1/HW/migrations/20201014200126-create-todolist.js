ee6d8252ba257d3fee527ef34ce67866e91a407b
Robert <r.powelliv@gmail.com>
2020-10-14 16:21:30 -0400

    SQl

---
+++ week15/1-tuesday/class/exercise1/HW/migrations/20201014200126-create-todolist.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('todolists', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      todoItem: {
+        type: Sequelize.TEXT
+      },
+      isCompleted: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('todolists');
+  }
+};
