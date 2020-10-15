fde644da7173db472af4874a229bf5fdbb57dd37
ejw773 <ejw773@gmail.com>
2020-10-15 16:02:46 -0400

    adds sequelize-cli

---
+++ week15/2-thursday/todo-elijah/migrations/20201015174859-create-to-do.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('ToDos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      toDo: {
+        type: Sequelize.STRING
+      },
+      completed: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('ToDos');
+  }
+};
---
+++ week15/2-thursday/todo-elijah/models/todo.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class ToDo extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  ToDo.init({
+    toDo: DataTypes.STRING,
+    completed: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'ToDo',
+  });
+  return ToDo;
+};
