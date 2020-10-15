1fa3715f908391b36087398d42273601184deb3e
Stacey Graham <stacey.graham1106@gmail.com>
2020-10-14 23:53:34 -0400

    added click event handler function for the ul to handle a click on delete btn or check btn

--- week12/3-saturday/class/exercise2/public/index.js
+++ week12/3-saturday/class/exercise2/public/index.js
@@ -1,10 +1,10 @@
 console.log("Hello World")
 $().ready(()=> {
     // fetch('localhost:3000/api/')
-    
+
 //Selectors
 document.querySelector('form').addEventListener('submit', handleSubmitForm);
-
+document.querySelector('ul').addEventListener('click', handleClickDeleteOrCheck);
 //Event Handlers
 function handleSubmitForm(e) {
     e.preventDefault();
@@ -21,15 +21,11 @@ function addTodo(todo) {
     li.innerHTML = `
         <span class="todo-item">${todo}</span>
         <button name="checkButton"><i class="fas fa-check-square"></i></button>
-        <button name="deleteButton" ><i class="fas fa-trash"></i></button>
+        <button name="deleteButton"><i class="fas fa-trash"></i></button>
     `;
     li.classList.add('todo-list-item');
     ul.appendChild(li);
 }
-
-
-
-
 
 
 
