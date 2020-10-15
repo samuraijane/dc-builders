4ac784180fce53bc20f8eefb332e188a7f81d789
Robert <r.powelliv@gmail.com>
2020-10-15 18:49:03 -0400

    SQL

---
+++ week15/1-tuesday/class/exercise1/HW/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+
+const todo = require('./router/todo')
+
+const db = require('./models')
+
+db.sequelize.authenticate()
+    .then( () => {
+        console.log("Database connected")
+    }) 
+    .catch( () =>{
+        console.log("Database not connected!!!")
+    })
+
+db.sequelize.sync()
+
+app.use(bodyParser.json())
+
+app.use('/api', todo)
+
+app.listen(3000, () => {
+    console.log('Server started')
+});
