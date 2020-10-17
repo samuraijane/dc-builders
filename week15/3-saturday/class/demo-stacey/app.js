66dd87d592bbe40b78f69b3f43717e9c65b097db
Stacey Graham <stacey.graham1106@gmail.com>
2020-10-17 19:17:15 -0400

    adding updates

---
+++ week15/3-saturday/class/demo-stacey/app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
+
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.use(cookieParser('super secret'))
+app.use(session({
+    secret: 'super secret',
+    cookie: {maxAge: 60000}
+}))
+
+app.get('/', (req, res) => {
+    console.log(req.session)
+    console.log("Hello world")
+    res.send(`<h1>Hello world from server</h1>
+    <h2>Session</h2>
+    <pre>${JSON.stringify(req.session, null, '\t')}</pre>
+    <h2>Unsigned Cookies</h2>
+    <pre>${JSON.stringify(req.cookies, null, '\t')}</pre>
+    <h2>Signed Cookies</h2>
+    <pre>${JSON.stringify(req.signedCookies, null, '\t')}</pre>`)
+})
+
+app.get('/setmyusername', (req, res) => {
+    req.session.username = "Harry Styles"
+    res.send(`<h1>You username is now Harry</h1>`)
+})
+
+app.get('/setcookie', (req, res) => {
+    res.cookie('password', 'cupcakes')
+    res.send('I sent your cookies')
+})
+
+app.listen(3000, () => {
+    console.log("Server started") 
+})
