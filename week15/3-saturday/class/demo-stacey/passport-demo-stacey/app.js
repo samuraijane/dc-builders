cd4318042b18dd49072811f58a7ff4fbc82f358f
Stacey Graham <stacey.graham1106@gmail.com>
2020-10-18 22:43:40 -0400

    created a model and a user table

--- week15/3-saturday/class/demo-stacey/passport-demo-stacey/app.js
+++ week15/3-saturday/class/demo-stacey/passport-demo-stacey/app.js
@@ -4,13 +4,37 @@ const bodyParser = require('body-parser')
 const session = require('express-session')
 const passport = require('passport')
 const GitHubStrategy = require('passport-github').Strategy
-const { Sequelize } = require ('sequelize');
+const { Sequelize , DataTypes, Model } = require ('sequelize');
 
 //Setting up my Sequelize here
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS,{
     host: 'localhost',
     dialect: 'postgres'
   });
+
+  // 2. Make a model!
+class User extends Model {}
+
+User.init({
+  // Model attributes are defined here
+  ghId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING
+    // allowNull defaults to true
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  // Other model options go here
+  sequelize, // We need to pass the connection instance
+  modelName: 'User' // We need to choose the model name
+});
+
 //Checkig connetion 
 //   sequelize.authenticate().then( ()=>{
 //     console.log('Connection has been established successfully.')
@@ -37,17 +61,34 @@ passport.use(new GitHubStrategy({
     callbackURL: process.env.GH_CALLBACK
   },
 
-  //THis is the function that reurns a user profile after someone
-  //;logs into github. You should save the profile to your database HERE
-  function(accessToken, refreshToken, profile, cb) {
-      console.log(JSON.stringify(profile))
+    // This is the function that returns a user profile after someone
+  // logs into github. You should save the profile to your database HERE
+  async function(accessToken, refreshToken, profile, cb) {
+    console.log("****PROFILE\n" + JSON.stringify(profile))
 
-    //YOUR database CODE HERE
+    // ASIDE: Access Tokens are super important! Treat them like passwords (never store in plain text)
+    // You can use this to talk to the Github API
+    console.log("\nAccess Token: " + accessToken)
+    
+    // YOUR DB CODE HERE
+    // 3. Use the model here - store the use info
+    let user = await User.findOne({where: {ghId: parseInt(profile.id)  }})
 
-    //Tells passport to move on
-    cb(null, profile)
-  }
-));
+    if(!user) {
+        // User doesn't exist - make a new database entry
+        user = await User.build({
+          ghId: parseInt(profile.id),
+          username: profile.username,
+          createAt: new Date(),
+          updatedAt: new Date()
+        })
+      }
+      // Tells passport to move on
+      cb(null, user)
+    }
+  ));
+
+sequelize.sync()
 
 app.use(passport.initialize())
 app.use (passport.session())
@@ -77,14 +118,36 @@ app.get('/logout', (req, res) => {
 })
 
 
+// Homepage
 app.get('/', (req, res) => {
     res.send(`<h1>Hello world from server</h1>
-    <h2>Let's Login</h2>
-    <button>Login</button>
-    <pre>${JSON.stringify(req.session, null, '\t')}</pre>`)
+    <a href="./auth/github">Login</a>
+    <br />
+    <a href="./logout">Logout</a>
+    <h2>Session</h2>
+    <pre>${JSON.stringify(req.session, null, '\t')}</pre>
+    <h2>Req.user</h2>
+    <pre>${JSON.stringify(req.user, null, '\t')}</pre>`)
 })
 
+// NEW! Didn't cover this in class 
+// How can we make sure that someone is logged in to see a page?
+// We make our own middleware we can add to our routes.
 
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/')
+}
+
+// This is a secret page that only logged in users can see!
+app.get('/fansonly', ensureAuthenticated,(req, res) => {
+    console.log(req.session)
+    res.send(`<h1> You're Special! </h1> <img src="https://media.giphy.com/media/TfumiIU3zb7K1zJoQD/giphy.gif" />`)
+})
+
+// END NEW STUFF
 
 app.listen(process.env.PORT, ()=> {
     console.log("Server Running on port: " + process.env.PORT)
