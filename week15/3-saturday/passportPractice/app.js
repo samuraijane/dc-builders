d794b2885e61a67a56dc451b700f0041c7224f7d
Heather Luttrell <possumdiva@heatherluttrell.com>
2020-10-20 17:55:13 -0400

    classwork/homework from saturday

--- week15/3-saturday/passportPractice/app.js
+++ week15/3-saturday/passportPractice/app.js
@@ -1,14 +1,14 @@
+// App.js
+
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const passport = require("passport");
-const path = require("path");
 const GitHubStrategy = require("passport-github").Strategy;
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes, Model } = require("sequelize");
 
-// step 1.setting up sequelize here
-
+// 1. Setting up my Sequelize here
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -18,12 +18,39 @@ const sequelize = new Sequelize(
     dialect: "postgres",
   }
 );
-//check that database can connect password and user are right
-// sequelize.authenticate().then(() => {
-//   console.log("database connected- hurray!");
-// });
 
-//step 2. make a model
+// // Check that the database can connect (password and user are right)
+// sequelize.authenticate().then( ()=>{
+//     console.log("Database connected - hurray!")
+// }).catch( (err) => {
+//     console.log(err)
+// })
+
+// 2. Make a model!
+class User extends Model {}
+
+User.init(
+  {
+    // Model attributes are defined here
+    ghId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      // allowNull defaults to true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    // Other model options go here
+    sequelize, // We need to pass the connection instance
+    modelName: "User", // We need to choose the model name
+  }
+);
 
 // Set up the in express
 const app = express();
@@ -49,13 +76,29 @@ passport.use(
 
     // This is the function that returns a user profile after someone
     // logs into github. You should save the profile to your database HERE
-    function (accessToken, refreshToken, profile, cb) {
-      console.log(JSON.stringify(profile));
+    async function (accessToken, refreshToken, profile, cb) {
+      console.log("****PROFILE\n" + JSON.stringify(profile));
+
+      // ASIDE: Access Tokens are super important! Treat them like passwords (never store in plain text)
+      // You can use this to talk to the Github API
+      console.log("\nAccess Token: " + accessToken);
 
       // YOUR DB CODE HERE
+      // 3. Use the model here - store the use info
+      let user = await User.findOne({ where: { ghId: parseInt(profile.id) } });
 
+      if (!user) {
+        // User doesn't exist - make a new database entry
+        user = await User.build({
+          ghId: parseInt(profile.id),
+          username: profile.username,
+          createAt: new Date(),
+          updatedAt: new Date(),
+        });
+        await user.save();
+      }
       // Tells passport to move on
-      cb(null, profile);
+      cb(null, user);
     }
   )
 );
@@ -73,6 +116,7 @@ passport.deserializeUser(function (id, done) {
   //This is looking up the User in the database using the information from the session "id"
 });
 
+// Callback
 app.get(
   "/auth/github/callback",
   passport.authenticate("github", { failureRedirect: "/" }),
@@ -80,6 +124,7 @@ app.get(
     res.redirect("/");
   }
 );
+
 // This is the route I call when I want to login
 app.get("/auth/github", passport.authenticate("github"));
 
@@ -89,15 +134,38 @@ app.get("/logout", (req, res) => {
   res.redirect("/");
 });
 
+// Homepage
 app.get("/", (req, res) => {
-  //   res.sendFile(path.join(__dirname + "/index.html"));
-  // });
-
-  res.send(`<h1 id="loggedIn"></h1>
-  <form id="gitLog"><button type="submit">Login with Github</button></form>
-  <button>Logout</button>
-  <pre>${JSON.stringify(req.session, null, "\t")}</pre>`);
+  res.send(`<h1>Hello world from server</h1>
+    <a href="./auth/github">Login</a>
+    <br />
+    <a href="./logout">Logout</a>
+    <h2>Session</h2>
+    <pre>${JSON.stringify(req.session, null, "\t")}</pre>
+    <h2>Req.user</h2>
+    <pre>${JSON.stringify(req.user, null, "\t")}</pre>`);
 });
+
+// NEW! Didn't cover this in class
+// How can we make sure that someone is logged in to see a page?
+// We make our own middleware we can add to our routes.
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
+
+// This is a secret page that only logged in users can see!
+app.get("/fansonly", ensureAuthenticated, (req, res) => {
+  console.log(req.session);
+  res.send(
+    `<h1> You're Special! </h1> <img src="https://media.giphy.com/media/TfumiIU3zb7K1zJoQD/giphy.gif" />`
+  );
+});
+
+// END NEW STUFF
 
 app.listen(process.env.PORT, () => {
   console.log("Server Running on port: " + process.env.PORT);
