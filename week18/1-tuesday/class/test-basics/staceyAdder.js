2f3c39d624100d26f81e7fd3bdc6e88770132323
Stacey Graham <stacey.graham1106@gmail.com>
2020-11-17 18:23:36 -0500

    adding class demo exercises

--- week18/1-tuesday/class/test-basics/staceyAdder.js
+++ week18/1-tuesday/class/test-basics/staceyAdder.js
@@ -1,8 +1,23 @@
 module.exports = dansAdder;
 //Stacey's Special addition function, adds two numbers
+//First function for test #1
+// function dansAdder(a, b) {
+//     return a + b
+// }
+
+//2nd function for test #2
+// function dansAdder(a, b) {
+//     return parseInt(a) + parseInt(b)
+// }
+
+//3rd function for test #3
 function dansAdder(a, b) {
-    return a + b
+    if(!b) b=0;
+    return parseInt(a) + parseInt(b)
 }
+
+
+
 
 //Old school debugging
 
--- week18/1-tuesday/class/test-basics/staceyAdder.test.js
+++ week18/1-tuesday/class/test-basics/staceyAdder.test.js
@@ -1,7 +1,16 @@
 //This file is a Test Suite
 const dansAdder = require('./staceyAdder');
-//Test Case here
+//Test Case #1 here
 test("one plus two equals three", ()=> {
     expect(dansAdder(1,2)).toBe(3);
 })
 
+//test #2
+test("strings: one plus two equals three", ()=> {
+    expect(dansAdder("1","3")).toBe(4);
+})
+
+//test #3
+test ("strings: one plus nothing", ()=> {
+    expect(dansAdder("1", undefined)).toBe(1);
+})
