b558c2e31570746dd9fff6b7febdb2d5fb0a0462
ejw773 <ejw773@gmail.com>
2020-11-18 22:04:02 -0500

    adds student answers to exercises

--- week19/1-tuesday/class/exercise1/01-first-component/app.js
+++ week19/1-tuesday/class/exercise1/01-first-component/app.js
@@ -1,5 +1,9 @@
 let FirstComponent = () => {
-    return <span>Make this component render like the Windows Logo: <a href="https://www.channelweb.co.uk/api/v1/wps/993bb81/daa6fb74-bb2f-48c9-a399-464612d8252a/2/windows-logo-580x358.jpg" target="_blank">https://www.channelweb.co.uk/api/v1/wps/993bb81/daa6fb74-bb2f-48c9-a399-464612d8252a/2/windows-logo-580x358.jpg</a></span>
-}
+    return (
+        <body className="App">
+            <img src="https://www.channelweb.co.uk/api/v1/wps/993bb81/daa6fb74-bb2f-48c9-a399-464612d8252a/2/windows-logo-580x358.jpg" />
+        </body>
+    );
+};
 
 ReactDOM.render(<FirstComponent />, document.getElementById('root'))
